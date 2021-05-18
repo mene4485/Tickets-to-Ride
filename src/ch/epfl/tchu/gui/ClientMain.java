690300ac5a4714422f685146c031dd240bed0514
Menelik <meneliknouvellon@gmail.com>
2021-05-18 10:42:14 +0200

    18/05 10:42

--- src/ch/epfl/tchu/gui/ClientMain.java
+++ src/ch/epfl/tchu/gui/ClientMain.java
@@ -34,7 +34,7 @@ public class ClientMain extends Application {
 
         RemotePlayerClient playerClient =
                 new RemotePlayerClient(new GraphicalPlayerAdapter(),
-                        "128.179.167.180",
+                        hostName,//"128.179.167.180",
                         port);
 
         new Thread(playerClient::run).start();
