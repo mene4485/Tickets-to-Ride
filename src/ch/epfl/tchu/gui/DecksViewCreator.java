48712620c0df919cd15e67f68dddce2ddd450c6b
alberttkt <troussardalbert@gmail.com>
2021-05-22 17:24:23 +0200

    22/05 17:30

--- src/ch/epfl/tchu/gui/DecksViewCreator.java
+++ src/ch/epfl/tchu/gui/DecksViewCreator.java
@@ -144,8 +144,8 @@ class DecksViewCreator {
 
             stackPane.setOnMouseClicked(e -> {
                 cardHandler.get().onDrawCard(j);
-                cardChoosingSound.start();
-                cardChoosingSound.setMicrosecondPosition(0);
+         //       cardChoosingSound.start();
+           //     cardChoosingSound.setMicrosecondPosition(0);
             });
 
 
@@ -161,8 +161,8 @@ class DecksViewCreator {
         buttonGraphicCard.setText(StringsFr.CARDS);
         buttonGraphicCard.setOnMouseClicked(e -> {
             cardHandler.get().onDrawCard(Constants.DECK_SLOT);
-            cardChoosingSound.start();
-            cardChoosingSound.setMicrosecondPosition(0);
+        //    cardChoosingSound.start();
+        //    cardChoosingSound.setMicrosecondPosition(0);
         });
 
         buttonGraphicCard.disableProperty().bind(cardHandler.isNull());
